fde1aef46e83e171c8ac78d0921217de73682ee7
pkvstar <pk8649726@gmail.com>
2025-06-22 12:59:52 +0530

    two_Bit_Manipulation

--- Bit_Manipulation/two(More_Questions)/two.java
+++ Bit_Manipulation/two(More_Questions)/two.java
@@ -3,7 +3,12 @@ public class two{
         // swap(3,5);
         // System.out.println(checkIthBit(13,1));
         // System.out.println(setIthBit(9,2));
-        System.out.println(clearIthBit(7,1));
+        // System.out.println(clearIthBit(7,1));
+        // System.out.println(toggleIthBit(13,2));
+        // System.out.println(clearLastSetBit(40));
+        // System.out.println(powerOfTwo(32));
+        System.out.println(countSetBits(3));
+
     }
     //? 1. swap two numbers
     //* Approach : we know that num ^ num = 0 
@@ -41,6 +46,44 @@ public class two{
     //? 4. Clear the ith Bit
     //* Approach : left shift 1 ith time and use '^' (XOR) operator with num
     public static int clearIthBit(int num,int i){
-        return (1<<i)^num;
+        return (1<<i)^num; //* OR num & ~(1<<i)
+    }
+
+    //? 5. Toggle the ith Bit
+    //* Approach : left shift 1 ith time and use '^'(XOR) operator with num
+    public static int toggleIthBit(int num,int i){
+        return num^(1<<i);
+    }
+
+    //? 6. Clear the Last Set Bit(rightmost)
+    //* Approach : method 1(Brute Force) -> find position of rightmost set bit
+    //* and clear that ith bit
+    //* method 2(Optimal) -> n&(n-1)
+    public static int clearLastSetBit(int num){
+        return num&(num-1);
+    } 
+
+    //? 7. Check if the Number is a Power of 2
+    //* Approach : method 1(Brute Force) -> count set bits and if count is 1 means
+    //* it is the number is a power of 2 and if count is greater than that then means false
+    //* method 2(Optimal) -> n&(n-1); if it returns 0 ; it means there was only one set bit
+    //* which shows that it was the number power of two and if it return any number ; it means
+    //* that number as having more than one set bit which is not power of two
+    public static boolean powerOfTwo(int num){
+        return (num&(num-1))==0 ? true : false;
+    }
+
+    //? 8. Count the no. of Set Bits
+    //* Approach : method 1(Brute Force) -> right shift the number and use '&' with 1
+    //* if it returns 1 then increase count and do this work till num!=0
+    //* method 2(Optimal) -> n&(n-1) till n!=0 and count++ everytime loop runs because
+    //* it remove right most bit every time and also pass new n for every iteration
+    public static int countSetBits(int num) {
+        int count = 0;
+        while(num!=0){
+            num = num&(num-1);
+            count++;
+        }
+        return count;
     }
 }
